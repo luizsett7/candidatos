933474a617cafe80129d9bcc8cec30694c2757fe
luizsett7 <luizsett7@gmail.com>
2021-02-15 08:19:32 -0300

    Insersao do Materialize

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/login/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/login',
       name: 'Login',
